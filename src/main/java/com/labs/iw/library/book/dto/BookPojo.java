ba45c02a2397c7acbf71b292d9f1e63c916116ec
Mario Petrovski <mpetrovski@twycis.com>
2023-02-27 11:08:09 +0100

    Upgrate spring boot version in 2.7.9

--- src/main/java/com/labs/iw/library/book/dto/BookPojo.java
+++ src/main/java/com/labs/iw/library/book/dto/BookPojo.java
@@ -6,9 +6,13 @@ import com.labs.iw.library.infrastructure.pojo.BasePojo;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 public class BookPojo extends BasePojo {
+	@NotNull(message = "title value must not be null")
 	private String title;
+	@NotNull(message = "description value must not be null")
 	private String description;
 	private Date modifiedOn;
 }
