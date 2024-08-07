4eca50a682bbf13e50e83734443f905bf401d2c0
bojan_dukovski <bdukovski@twycis.com>
2024-08-07 17:06:38 +0200

    modified SwaggerConfig

--- src/main/java/com/labs/iw/library/config/SwaggerConfig.java
+++ src/main/java/com/labs/iw/library/config/SwaggerConfig.java
@@ -1,24 +1,29 @@
 package com.labs.iw.library.config;
 
+import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 
 @Configuration
-@EnableSwagger2
+@EnableWebMvc
 public class SwaggerConfig {
 
 	@Bean
-	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2)
-				.select()
-				.apis(RequestHandlerSelectors.any())
-				.paths(PathSelectors.any())
-				.build();
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI()
+				.info(new Info()
+						.title("API Documentation")
+						.version("1.0")
+						.description("API documentation for the CRUD template project using Spring Boot 3"));
+	}
+
+	@Bean
+	public OpenApiCustomizer customerGlobalHeaderOpenApiCustomizer() {
+		return openApi -> openApi.getPaths().values().forEach(pathItem -> pathItem.readOperations().forEach(operation -> {
+			operation.addTagsItem("default");
+		}));
 	}
 }
