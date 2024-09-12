6658ebbfe44156c37837a4a944b2382e3f3a4ef1
Nikola Popovski <nikola.popovski@iwconnect.com>
2024-09-12 09:36:18 +0200

    Remove EnableJpaRepositories annotation

--- src/main/java/com/labs/iw/library/Application.java
+++ src/main/java/com/labs/iw/library/Application.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories("com.labs.iw.library.book.repository")
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
